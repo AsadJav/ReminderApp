789d686bb254a6c4043930ebf60625526dd10f90
Asad Javed <asadjaved9490@gmail.com>
2023-06-08 10:22:40 +0500

    Completed and Removed Extra

--- app/src/Redux/Store.js
+++ app/src/Redux/Store.js
@@ -1,23 +1,7 @@
-import {Iterable} from 'immutable';
-import {
-  combineReducers,
-  configureStore,
-  createSerializableStateInvariantMiddleware,
-  isPlain,
-} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import {persistStore, persistReducer} from 'redux-persist';
 import ReminderSlice from './ReminderSlice';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const isSerializable = value => Iterable.isIterable(value) || isPlain(value);
-
-const getEntries = value =>
-  Iterable.isIterable(value) ? value.entries() : Object.entries(value);
-
-const serializableMiddleware = createSerializableStateInvariantMiddleware({
-  isSerializable,
-  getEntries,
-});
 
 const rootReducer = combineReducers({
   reminders: ReminderSlice,
@@ -29,10 +13,8 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-//export const store = createStore(persistedReducer);
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: [serializableMiddleware],
 });
 
 export const persistor = persistStore(store);
